1c7713b7349f0171c68273409a7a5517ef4ffd89
doctor.madbee@gmail.com <Password@12>
2021-05-30 11:35:33 +0700

    first commit

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 
 def home(requests):
     return render(requests, 'home.html')
+
+
+def reverse(requests):
+    text = requests.GET['text_to_reverse']
+    len_text = len(text.split())
+    rev_text = text[::-1]
+    return render(requests, 'reversed.html', {
+        'text': text,
+        'len_text': len_text,
+        'rev_text': rev_text
+    })
